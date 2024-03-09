94853b2f90fea801b612fd61e76e74689cc45a2f
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-03-09 13:58:26 +0700

    start project

---
+++ src/elements/navbar.jsx
@@ -0,0 +1,12 @@
+import { Link} from "react-router-dom";
+
+const navbar = () =>{
+   return (
+      <div>
+         <div className="bg-blue-500">Jajanan</div>
+
+      </div>
+   )
+}
+
+export default navbar;
