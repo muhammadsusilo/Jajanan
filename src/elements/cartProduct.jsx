808ba9771e365e0a185abad23b9774258a4145e2
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-03-09 20:47:15 +0700

    component in makanan

--- src/elements/cartProduct.jsx
+++ src/elements/cartProduct.jsx
@@ -9,32 +9,47 @@ const CartProduct = (props) => {
    )
 }
 
-const HeaderCart = () => {
+const HeaderCart = (props) => {
+   const {image, alt}= props;
 return (
    <div>
-
+         <div className="">
+            <img src={image} alt={alt}
+            className="rounded-t-lg object-cover"
+            />
+         </div>
    </div>
 )
 
 }
 
-const BodyCart = () => {
-return (
-   <div>
-
-   </div>
-)
+const BodyCart = (props) => {
+   const {nama, description} = props;
+   return (
+      <div>
+         <div className="py-3 pl-3">
+            <p>{nama}</p>
+            <p>{description}</p>
+         </div>
+      </div>
+   )
 }
 
-const FooterCart = () => {
-return (
-   <div>
-      
-   </div>
-)
+const FooterCart = (props) => {
+   const {price} = props;
+   return (
+      <div>
+         <div className="flex justify-evenly pb-4 items-center ">
+            <p>{price}</p>
+            <Button>Detail</Button>
+            <Button>Cart</Button>
+         </div>
+      </div>
+   )
 }
 
-
-
+CartProduct.HeaderCart = HeaderCart;
+CartProduct.BodyCart = BodyCart;
+CartProduct.FooterCart = FooterCart;
 
 export default CartProduct;
