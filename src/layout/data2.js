ce19e16eaf1ae491ab84be1785a7ef451a2c4640
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-04-05 19:34:22 +0700

    revisi harga

--- src/layout/data2.js
+++ src/layout/data2.js
@@ -1,32 +1,32 @@
 const makanJajan = [{
     m_id: 1,
     title: "Kue Lumpur",
-    price: 17000,
-    image: "../../public/assets/img/kuelumpur.jpg",
+    price: 12000,
+    image: "../../public/img/kuelumpur.jpg",
     description: `Kue Lumpur adalah kue tradisional Indonesia yang terbuat dari tepung beras, kelapa parut, gula merah, dan beberapa bahan lainnya. Biasanya dimasak dengan cara dipanggang atau dikukus. Kue Lumpur memiliki tekstur yang lembut dan rasa manis gurih.`
 }, {
     m_id: 2,
     title: "Getuk",
-    price: 17000,
-    image: "../../public/assets/img/getuk.jpg",
+    price: 15000,
+    image: "../../public/img/getuk.jpg",
     description: `Getuk adalah jajanan tradisional Jawa yang terbuat dari ketela pohon yang dihaluskan dan kemudian dicampur dengan gula pasir. Getuk biasanya dihidangkan dengan taburan kelapa parut.`
 }, {
     m_id: 3,
     title: "Kue Cucur",
-    price: 17000,
-    image: "../../public/assets/img/kuecucur.jpg",
+    price: 3000,
+    image: "../../public/img/kuecucur.jpg",
     description: `Kue Cucur adalah kue tradisional Indonesia yang berbahan dasar tepung terigu, gula, dan air kelapa. Kue ini memiliki tekstur yang renyah di bagian luar dan lembut di bagian dalam.`
 }, {
     m_id: 4,
     title: "Klepon",
-    price: 17000,
-    image: "../../public/assets/img/klepon.jpg",
+    price: 1000,
+    image: "../../public/img/klepon.jpg",
     description: `Klepon adalah jajanan khas Jawa yang terbuat dari tepung beras yang dibentuk bulat dan diisi dengan gula merah cair. Klepon kemudian direbus dan digulingkan dalam kelapa parut.`
 }, {
     m_id: 5,
     title: "Cenil",
-    price: 17000,
-    image: "../../public/assets/img/cenil.jpg",
+    price: 3000,
+    image: "../../public/img/cenil.jpg",
     description: `Cenil adalah jajanan tradisional Indonesia yang terbuat dari tepung tapioka yang dicampur dengan air,
    dibentuk bulat kecil,
    dan direbus.Setelah matang,
@@ -34,32 +34,32 @@ const makanJajan = [{
 }, {
     m_id: 6,
     title: "Es Kelapa Muda",
-    price: 17000,
-    image: "../../public/assets/img/eskelapa.jpg",
+    price: 5000,
+    image: "../../public/img/eskelapa.jpg",
     description: `Minuman yang terbuat dari air kelapa muda yang dicampur dengan es dan kadang-kadang diberi tambahan sirup atau buah.`
 }, {
     m_id: 7,
     title: "Es Teler",
-    price: 17000,
-    image: "../../public/assets/img/esteler.jpg",
+    price: 6000,
+    image: "../../public/img/esteler.jpg",
     description: `Minuman yang terdiri dari campuran potongan buah seperti alpukat, kelapa muda, nangka, es serut, yang kemudian dituangi dengan susu kental manis dan sirup.`
 }, {
     m_id: 8,
     title: "Wedang Jahe",
-    price: 17000,
-    image: "../../public/assets/img/wedangjahe.jpg",
+    price: 5000,
+    image: "../../public/img/wedangjahe.jpg",
     description: `Minuman hangat yang terbuat dari jahe segar yang direbus dengan air dan ditambah gula aren atau gula merah. Biasanya disajikan saat cuaca dingin atau sebagai obat tradisional untuk menghangatkan tubuh.`
 }, {
     m_id: 9,
     title: "Es Cendol",
-    price: 17000,
-    image: "../../public/assets/img/escendol.jpg",
+    price: 5000,
+    image: "../../public/img/escendol.jpg",
     description: `Minuman manis yang terbuat dari campuran tepung beras, santan, gula merah cair, dan adonan hijau yang terbuat dari tepung ketan. Biasanya disajikan bersama es serut.`
 }, {
     m_id: 10,
     title: "Es Doger",
-    price: 17000,
-    image: "../../public/assets/img/esdoger.jpg",
+    price: 7000,
+    image: "../../public/img/esdoger.jpg",
     description: `Minuman dingin yang terbuat dari campuran kelapa parut, santan, tape ketan hitam, dan sirup merah. Biasanya disajikan bersama es serut.`
 }, ]
 
